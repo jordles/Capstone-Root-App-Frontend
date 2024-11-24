24e1f4135c93e7245d6da7288937ce9a082518fa
Jacky Yang <jackyyang1670@gmail.com>
2024-11-24 04:38:28 -0800

    Added styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-
+import RootLogo from './RootLogo';
 function Navbar() {
   return (
     <nav className="navbar">
+      <Link to="/feed">
+        <RootLogo />
+      </Link>
       <Link to="/">Home</Link>
       <Link to="/color-picker">Color Picker</Link>
       <Link to="/login">Login</Link>
---
+++ src/components/RootLogo.jsx
@@ -0,0 +1,13 @@
+import PostButton from "./PostButton";
+
+const RootLogo = () => {
+  return (
+    <PostButton
+      icon="eco"
+      label="Home"
+      className="root-logo"
+    />
+  );
+};
+
+export default RootLogo;
